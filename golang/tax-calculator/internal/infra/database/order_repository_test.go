3bccd5c8b326534916cef802b4695ccac2eef41f
Nicole Calderari <nicole_calderari@hotmail.com>
2023-03-09 18:15:40 -0300

    test: test suite II

---
+++ golang/tax-calculator/internal/infra/database/order_repository_test.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"aula1/internal/entity"
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	//"github.com/mattn/go-sqlite3" bugado, estou usando mas fala que não
+)
+
+type OrderRepositoryTestSuite struct {
+	suite.Suite
+	Db *sql.DB
+}
+
+func (suite *OrderRepositoryTestSuite) SetupSuite() {
+	db, err := sql.Open("sqlite3", ":memory:")
+	suite.NoError(err)
+	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL)")
+	suite.Db = db
+}
+
+func (suite *OrderRepositoryTestSuite) TearDownSuite() {
+	suite.Db.Close()
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(OrderRepositoryTestSuite))
+}
+
+func (suite *OrderRepositoryTestSuite) TestSave() {
+	order, err := entity.NewOrder("111", 10.0, 1.0)
+	suite.NoError(err)
+	suite.NoError(order.CalculateFinalPrice())
+	repo := NewOrderRepository(suite.Db)
+	err = repo.Save(order)
+	suite.NoError(err)
+}
